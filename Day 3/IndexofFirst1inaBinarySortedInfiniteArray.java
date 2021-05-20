954d87c7d13d8494f46ffc8235ab7d140ba619c6
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-05-20 16:01:43 +0530

    time o(logn) || space o(1)

---
+++ Day 3/IndexofFirst1inaBinarySortedInfiniteArray.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public long firstIndex(long arr[], long n)
+    {
+        // edge case
+        if(n == 1)
+            return arr[0] == 1 ? 0 : -1;
+        
+        long key = 1;
+        int start = 0;
+        int end = arr.length - 1;
+        
+        // finding the range in which we can get the answer;
+        while(count < n && 1 > arr[end]){
+            start = end;
+            end = end * 2;
+        }
+        
+        // apply first occurence algorithm to find our answer;
+        
+        int res = -1;
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            
+            if(key == arr[mid]){
+                res = mid;
+                end = mid - 1;
+            }
+            
+            else
+                start = mid + 1;
+        }
+        
+        return res;
+        
+    }
+}
