a01e0c6ec61e6ea835796dc7d2b9ca2de1418b1b
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-05-23 23:54:23 +0530

    Create minStack.java

---
+++ Day 6/minStack.java
@@ -0,0 +1,27 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int peek = stack.pop();
+        if (peek == min){
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
