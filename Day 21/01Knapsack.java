6a2d803a076a782d3a00b97b609bba4465a1d007
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-08 19:32:21 +0530

    Create 01Knapsack.java

---
+++ Day 21/01Knapsack.java
@@ -0,0 +1,26 @@
+
+class Solution 
+{ 
+    static int knapSack(int w, int wt[], int val[], int n) 
+    { 
+         int t[][] = new int[n+1][w+1];
+         
+         for(int i=0; i<n+1; i++){
+             for(int j=0; j<w+1; j++){
+                 if(i==0 || j==0){
+                    t[i][j] = 0;
+                    continue;
+                 }
+                    
+                if(wt[i-1] <= j)
+                    t[i][j] = Math.max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+                else
+                    t[i][j] = t[i-1][j];
+             }
+         }
+         
+         return t[n][w];
+    } 
+}
+
+
