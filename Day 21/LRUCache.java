0cad3dce31bca149a103d399c57a2f9684f017aa
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-06-09 08:08:48 +0530

    get and put in O(1) time complexity && space O(n)
    Example 1:
    
    Input
    ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
    [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
    Output
    [null, null, null, 1, null, -1, null, -1, 3, 4]
    
    Explanation
    LRUCache lRUCache = new LRUCache(2);
    lRUCache.put(1, 1); // cache is {1=1}
    lRUCache.put(2, 2); // cache is {1=1, 2=2}
    lRUCache.get(1);    // return 1
    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
    lRUCache.get(2);    // returns -1 (not found)
    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
    lRUCache.get(1);    // return -1 (not found)
    lRUCache.get(3);    // return 3
    lRUCache.get(4);    // return 4

--- Day 21/LRUCache.java
+++ Day 21/LRUCache.java
@@ -1,72 +1,83 @@
-class Node {
-int key;
-int value;
-Node pre;
-Node next;
+// In this question I tried to implement an algorithm for which
+// recently used node will be at next to head
+// least recently used node will be at tail prev
 
-public Node(int key, int value) {
-	this.key = key;
-	this.value = value;
-}
-}
-public class LRUCache {
+// for implementing this algorithm I used hashmap for maping keys with their corresponding nodes
 
-HashMap<Integer, Node> map;
-int capicity, count;
-Node head, tail;
 
-public LRUCache(int capacity) {
-	this.capicity = capacity;
-	map = new HashMap<>();
-	head = new Node(0, 0);
-	tail = new Node(0, 0);
-	head.next = tail;
-	tail.pre = head;
-	head.pre = null;
-	tail.next = null;
-	count = 0;
+class LRUCache {
+    
+    // doubly linked list having key and value
+    class Node{
+        Node prev;
+        Node next;
+        int key;
+        int value;
+        
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    // map for maping keys with their corresponding nodes
+    HashMap<Integer, Node> map = new HashMap<>();
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+    int capacity;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        join(head, tail);
+    }
+    
+    private void join(Node n1, Node n2){
+        n1.next = n2;
+        n2.prev = n1;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key))
+            return -1;
+        
+        // if the key is present in the map 
+        // then that node of the corresponding key will be considered as recently used node
+        // so I will remove that node from its current position and move that node to next to head;
+        
+        Node node = map.get(key);
+        join(node.prev, node.next);     // removing node from its current position
+        moveToHead(node);               // joining node next to head;
+        
+        return node.value;
+    }
+    
+    private void moveToHead(Node node){
+        Node nextNode = head.next;
+        join(head, node);
+        join(node, nextNode);
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            join(node.prev, node.next);
+            moveToHead(node);
+            node.value = value;
+        } else{
+                // if we hit the capacity then its time to remove least recently used node from the map
+                // and add the new node into the map and doubly linked list;
+            
+            if(map.size() == capacity){
+                map.remove(tail.prev.key);      // removing the key or node from the map;
+                join(tail.prev.prev, tail);     // removing the node from the doubly linked list
+            }
+            
+            // adding a new node into the map and to doubly linked list;
+            Node node = new Node(key, value);
+            moveToHead(node);
+            map.put(key, node);
+        }
+    }
 }
 
-public void deleteNode(Node node) {
-	node.pre.next = node.next;
-	node.next.pre = node.pre;
-}
-
-public void addToHead(Node node) {
-	node.next = head.next;
-	node.next.pre = node;
-	node.pre = head;
-	head.next = node;
-}
-
-public int get(int key) {
-	if (map.get(key) != null) {
-		Node node = map.get(key);
-		int result = node.value;
-		deleteNode(node);
-		addToHead(node);
-		return result;
-	}
-	return -1;
-}
-
-public void set(int key, int value) {
-	if (map.get(key) != null) {
-		Node node = map.get(key);
-		node.value = value;
-		deleteNode(node);
-		addToHead(node);
-	} else {
-		Node node = new Node(key, value);
-		map.put(key, node);
-		if (count < capicity) {
-			count++;
-			addToHead(node);
-		} else {
-			map.remove(tail.pre.key);
-			deleteNode(tail.pre);
-			addToHead(node);
-		}
-	}
-}
-}
+ 
